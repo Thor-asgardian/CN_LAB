c550a812ab8c0c97488381e0e602e0e8af465c9d
Thor-asgardian <144478466+Thor-asgardian@users.noreply.github.com>
2023-11-01 01:02:00 -0700

    Update datagram_server.java

--- datagram_server.java
+++ datagram_server.java
@@ -1,23 +1,21 @@
 import java.net.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class DatagramServer {
     public static void main(String[] args) {
-        final int SERVER_PORT = 9876;
-
-        try (DatagramSocket socket = new DatagramSocket(SERVER_PORT)) {
-            System.out.println("Server is listening on port " + SERVER_PORT);
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(9876);
 
             while (true) {
                 byte[] receiveData = new byte[1024];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-                socket.receive(receivePacket);
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+
                 String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                
-                System.out.println("Received from client: " + message);
+                System.out.println("Client: " + message);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
