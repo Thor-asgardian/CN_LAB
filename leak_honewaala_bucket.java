ec31850abcdf9829fad4a7f4f2690d14c9350462
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2023-12-28 08:45:20 +0530

    Update leak_honewaala_bucket.java

--- leak_honewaala_bucket.java
+++ leak_honewaala_bucket.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String args[]) {
+public class CongestionControl {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int bucket_remaining = 0, sent, received;
         System.out.println("Enter the bucket capacity");
         int bucket_capacity = in.nextInt();
-        System.out.println("Enter the bucket rate (Rate at which the bucket sends the packets)");
+        System.out.println("Enter the bucket rate");
         int bucket_rate = in.nextInt();
         System.out.println("Enter the number of packets to be sent");
         int n = in.nextInt();
@@ -16,9 +16,9 @@ public class Main {
             buf[i] = in.nextInt();
         }
 
-        System.out.println(String.format("%s\t%s\t%s\t%s\t%s","Time_t","P_size","accepted","sent","remaining"));
+        System.out.println("Time_t\tP_size\taccepted\tsent\tremaining");
 
-        for (int i = 0; i < n; i++) { // Corrected: loop up to the number of packets, not the bucket capacity
+        for (int i = 0; i < n; i++) {
             if (buf[i] != 0) {
                 if (bucket_remaining + buf[i] > bucket_capacity) {
                     received = -1;
@@ -36,7 +36,7 @@ public class Main {
                     bucket_remaining = 0;
                 } else {
                     sent = bucket_rate;
-                    bucket_remaining -= bucket_rate; // Corrected: use -= for better readability
+                    bucket_remaining -= bucket_rate; 
                 }
             } else {
                 sent = 0;
@@ -64,7 +64,7 @@ Enter the packets sizes one by one
 4
 6
 
-TimeΔt	P_size	accepted	sent	remaining
+Time_t	P_size	accepted	sent	remaining
 1	        2	      2	        2	     0
 2	        3	      3	        2	     1
 3	        4	      4	        2	     3
