cbca72c535d2eef2f0736c5d5bbcefd3a65c588d
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-01-04 12:10:22 +0530

    Update transmission_of_ping.tcl

--- transmission_of_ping.tcl
+++ transmission_of_ping.tcl
@@ -1,31 +1,24 @@
-#Create Simulator
 set ns [new Simulator]
 
-#Open trace and NAM trace file
 set ntrace [open prog2.tr w]
 $ns trace-all $ntrace
 set namfile [open prog2.nam w]
 $ns namtrace-all $namfile
 
-#Finish Procedure
 proc Finish {} {
 global ns ntrace namfile
 
-#Dump all trace data and close the file
 $ns flush-trace
 close $ntrace
 close $namfile
 
-#Execute the nam animation file
 exec nam prog2.nam &
 
-#Find the number of ping packets dropped
 puts "The number of ping packets dropped are "
 exec grep "^d" prog2.tr | cut -d " " -f 5 | grep -c "ping" &
 exit 0
 }
 
-#Create six nodes
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
@@ -33,21 +26,17 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
-#Connect the nodes
 $ns duplex-link $n0 $n1 1Mb 10ms DropTail
 $ns duplex-link $n1 $n2 1Mb 10ms DropTail
 $ns duplex-link $n2 $n3 1Mb 10ms DropTail
 $ns duplex-link $n3 $n4 1Mb 10ms DropTail
 $ns duplex-link $n4 $n5 1Mb 10ms DropTail
 
-#Define the recv function for the class 'Agent/Ping'
 Agent/Ping instproc recv {from rtt} {
 $self instvar node_
-puts "node [$node_ id] received ping answer from $from with round trip time $rtt
-ms"
+puts "node [$node_ id] received ping answer from $from with round trip time $rtt ms"
 }
 
-#Create two ping agents and attach them to n(0) and n(5)
 set p0 [new Agent/Ping]
 $p0 set class_ 1
 $ns attach-agent $n(0) $p0
@@ -55,13 +44,9 @@ set p1 [new Agent/Ping]
 $ns attach-agent $n(5) $p1
 $ns connect $p0 $p1
 
-#Set queue size and monitor the queue
-#Queue size is set to 2 to observe the drop in ping packets
 $ns queue-limit $n(2) $n(3) 2
 $ns duplex-link-op $n(2) $n(3) queuePos 0.5
 
-#Create Congestion
-#Generate a Huge CBR traffic between n(2) and n(4)
 set tcp0 [new Agent/TCP]
 $tcp0 set class_ 2
 $ns attach-agent $n(2) $tcp0
@@ -69,13 +54,11 @@ set sink0 [new Agent/TCPSink]
 $ns attach-agent $n(4) $sink0
 $ns connect $tcp0 $sink0
 
-#Apply CBR traffic over TCP
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
 $cbr0 set rate_ 1Mb
 $cbr0 attach-agent $tcp0
 
-#Schedule events
 $ns at 0.2 "$p0 send"
 $ns at 0.4 "$p1 send"
 $ns at 0.4 "$cbr0 start"
@@ -86,5 +69,4 @@ $ns at 1.4 "$p0 send"
 $ns at 1.6 "$p1 send"
 $ns at 1.8 "Finish"
 
-#Run the Simulation
 $ns run
