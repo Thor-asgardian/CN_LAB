20956eb717799ef96241fc71bc13a16cf3a17789
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-01-08 22:32:34 +0530

    Create three-node_point-to-point_network.tcl

--- three-node_point-to-point_network.tcl
+++ three-node_point-to-point_network.tcl
@@ -5,28 +5,12 @@ $ns trace-all $ntrace
 set namfile [open prog1.nam w]
 $ns namtrace-all $namfile
 
-proc Finish {} {
-    global ns ntrace namfile
-
-    $ns flush-trace
-    close $ntrace
-    close $namfile
-
-    exec nam prog1.nam &
-    exec echo "The number of packet drops is " &
-    exec grep -c "^d" prog1.tr &
-    exit 0
-}
-
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 
 $ns duplex-link $n0 $n1 1Mb 10ms DropTail
 $ns duplex-link $n1 $n2 1Mb 10ms DropTail
-
-# $ns duplex-link-op $n0 $n1 orient right
-# $ns duplex-link-op $n1 $n2 orient right
 
 $ns queue-limit $n0 $n1 10
 $ns queue-limit $n1 $n2 10
@@ -39,6 +23,19 @@ $ns connect $tcp0 $sink0
 
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 attach-agent $tcp0
+
+proc Finish {} {
+    global ns ntrace namfile
+
+    $ns flush-trace
+    close $ntrace
+    close $namfile
+
+    exec nam prog1.nam &
+    exec echo "The number of packet drops is " &
+    exec grep -c "^d" prog1.tr &
+    exit 0
+}
 
 $ns at 0.0 "$cbr0 start"
 $ns at 5.0 "Finish"
